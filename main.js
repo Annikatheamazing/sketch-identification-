6ddf00580f8de626d1326eeaf5368d531f1a3cce
Annikatheamazing <annikaarora.6316@gmail.com>
2025-05-29 10:36:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,46 @@
-function setup(){
-    canavs = createCanvas(280, 280);
-    canavs.center();
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
     background("white");
     canvas.mouseReleased(classifyCanvas);
     synth = window.speechSynthesis;
-
-}
-
-
-function preload(){
+  }
+  
+  function preload() {
+  
+  
     classifier = ml5.imageClassifier('DoodleNet');
-}
-
-function clearCanvas(){
+  }
+  
+  
+  
+  function clearCanvas() {
+  
     background("white");
   }
-
-function draw(){
-    //Set stroke weight to 10
-    strokeWeight(10);
-    //Set stroke color to black
+  
+  function draw() {
+  
+    // Set stroke weight to 13
+    strokeWeight(13);
+    // Set stroke color to black
     stroke(0);
-    // If mouse is pressed , draw line between previous and current mouse positions
-    if(mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX , mouseY);
-    
+    // If mouse is pressed, draw line between previous and current mouse positions
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
     }
-}
-
-function classifyCanvas(){
+  }
+  
+  function classifyCanvas() {
     classifier.classify(canvas, gotResult);
-}
-
-function gotResult(results){
-
-    console.log(results);
-    document.getElementById('label').innerHTML = 'label:  ' + results[0].label;
-
-    document.getElementById('confidence').innerHTML = 'Confidence:  ' + Math.round(results[0].confidence * 100) + '%';
-
-
+  }
+  
+  function gotResult(results) {
+     console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+  
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+  
     utterThis = new SpeechSynthesisUtterance(results[0].label);
     synth.speak(utterThis);
-}
+  }
